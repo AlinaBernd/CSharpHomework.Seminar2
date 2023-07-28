f213d066a1da036454a1e6cc68ce55b8bf97b497
alina bernd <a-manaeva@mail.ru>
2023-07-28 22:39:33 +0300

    Добавила новый файл Program.cs и решение задачи 10

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+int ShowSecondNumber(int num)
+{
+    int tens = num / 10 % 10;
+        return tens;
+}
+int randNum = new Random().Next(100, 1000);
+int newNum = ShowSecondNumber(randNum);
+Console.WriteLine($"Second digit of {randNum} is {newNum}");
