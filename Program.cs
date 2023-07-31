94074a6c9db7d33e1400e492a3c291b5ba4385b4
alina bernd <a-manaeva@mail.ru>
2023-07-31 18:06:42 +0300

    Добавила в решение задачи 13

--- Program.cs
+++ Program.cs
@@ -12,3 +12,22 @@ int randNum = new Random().Next(100, 1000);
 int newNum = ShowSecondNumber(randNum);
 Console.WriteLine($"Second digit of {randNum} is {newNum}");
 
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа ( или -1, если третьей цифры нет).
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+int ShowThirdNumber(int num)
+{
+    if (num < 100)
+        return num = -1;
+    else
+    {
+        int thirdNumber = num /= 100;
+        return thirdNumber % 10;
+    }
+}
+int randNum = new Random().Next(1,1000);
+int newNum = ShowThirdNumber(randNum);
+Console.WriteLine($"Third digit of {randNum} is {newNum}");
